a3be10efe510d1534afc78089f03dbf8eaf502f9
Хоанг Хай <Hoang.H@edu.rea.ru>
2022-11-25 17:39:17 +0300

    upd

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var p = document.querySelector('p');
+var s = document.querySelector('strong');
 
 // if( /Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 //   p.innerText = 'ANDROID'
 // }
 
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  p.innerText = 'IOS'
+if( /iPhone|iPad|iPod/i.test(window.navigator.userAgent) ) {
+  s.innerText = 'IOS'
+} else {
+  s.innerText = 'ELSE'
 }
 
+p.innerText = window.navigator.userAgent;
 
-p.innerText = window.navigator.userAgent
 
 console.log(window.navigator.userAgent)
 console.log(typeof window.navigator.userAgent)
